9ad443ca8ee6105a88b4c561aacf74c71af3bd83
sean shirazi <sean@plumcake.engineer>
2016-04-28 11:19:01 +0300

    compatibility with Node 4.3

--- webpack-config-client.js
+++ webpack-config-client.js
@@ -33,18 +33,16 @@ config = {
         new webpack.DefinePlugin(GLOBALS),
         //new webpack.HotModuleReplacementPlugin(),
         //new webpack.NoErrorsPlugin()
-
-        ...(!DEV ? [
-            new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.UglifyJsPlugin({
-                compress: {
-                    screw_ie8: true,
-                    warnings: VERBOSE,
-                },
-            }),
-            new webpack.optimize.AggressiveMergingPlugin(),
-        ] : []),
-    ],
+    ].concat(!DEV ? [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                screw_ie8: true,
+                warnings: VERBOSE,
+            },
+        }),
+        new webpack.optimize.AggressiveMergingPlugin(),
+    ] : []),
     module: {
         loaders: [
             {
