35c7f0eca7d767fdab45b59eb868cc3f4e2edab5
sean shirazi <sean@plumcake.engineer>
2016-08-01 20:21:02 +0300

    wit.ai hack #99

--- src/server/deepiks-bot/ai.js
+++ src/server/deepiks-bot/ai.js
@@ -9,9 +9,15 @@ import gotAttachment from './got-attachment-action.js';
 import { Wit, log as witLog } from 'node-wit';
 import _ from 'lodash';
 import URL from 'url';
+import uuid from 'node-uuid';
 
 const { DB_TABLE_CONVERSATIONS, AI_ACTIONS_SERVER } = ENV;
 
+type WitData = {
+    context: Object,
+    sessionId: string,
+    lastActionPrefix?: string,
+};
 
 export function _mkClient(accessToken: string, respondFn: (m: ResponseMessage) => void) {
     return new Wit({
@@ -39,31 +45,34 @@ export function _mkClient(accessToken: string, respondFn: (m: ResponseMessage) =
     });
 }
 
-export async function _runActions(client: Wit, sessionId: string,
-                                  text: string, context: Object,
+export async function _runActions(client: Wit, text: string, witData: WitData,
                                   botParams: BotParams)
+                                  : Promise<WitData>
 {
-    let converseData = await client.converse(sessionId, text, context);
-    return await _runActionsHelper(client, sessionId, text, context,
+    let converseData = await client.converse(witData.sessionId, text, witData.context);
+    return await _runActionsHelper(client, text, witData,
                                    botParams, converseData, 5);
 }
 
-export async function _runActionsHelper(client: Wit, sessionId: string,
-                                        text: string, context: Object,
+export async function _runActionsHelper(client: Wit,
+                                        text: string,
+                                        witData: WitData,
                                         botParams: BotParams,
-                                        converseData: Object, level: number)
+                                        converseData: Object,
+                                        level: number)
+                                        : Promise<WitData>
 {
     console.log('_runActionsHelper: converseData: ', converseData);
     if (level < 0) {
         console.log('_runActionsHelper: Max steps reached, stopping.');
-        return context;
+        return witData;
     }
 
     if (!converseData.type) {
         throw new Error('Couldn\'t find type in Wit response');
     }
 
-    console.log('Context: ', context);
+    console.log('witData: ', witData);
     console.log('Response type: ', converseData.type);
 
     // backwards-cpmpatibility with API version 20160516
@@ -77,12 +86,12 @@ export async function _runActionsHelper(client: Wit, sessionId: string,
     }
 
     if (converseData.type === 'stop') {
-        return context;
+        return witData;
     }
 
     const requestData = {
-        sessionId,
-        context,
+        sessionId: witData.sessionId,
+        context: witData.context,
         text,
         entities: converseData.entities,
         publisherId: botParams.publisherId,
@@ -98,19 +107,39 @@ export async function _runActionsHelper(client: Wit, sessionId: string,
         if (invalidContext) {
             throw new Error('Cannot update context after \'send\' action');
         }
-        const newConverseData = await client.converse(sessionId, null, context);
-        return await _runActionsHelper(client, sessionId, text, context,
+        const newConverseData = await client.converse(witData.sessionId, null, witData.context);
+        return await _runActionsHelper(client, text, witData,
                                        botParams, newConverseData, level-1)
 
     } else if (converseData.type === 'action') {
-        const action = converseData.action;
+        let actionFullName = converseData.action;
+        let newWitData = witData;
+        let newRequestData = requestData;
+        const sepIndex = actionFullName.indexOf('_');
+        const prefix = sepIndex > 0 ? actionFullName.split('_')[0] : '';
+        const action = sepIndex > 0 ? actionFullName.substr(sepIndex+1) : actionFullName;
+        console.log(`prefix: ${prefix}`);
+        console.log(`action: ${action}`);
+        if (prefix !== witData.lastActionPrefix && (prefix || witData.lastActionPrefix)) {
+            console.log(`Story changed from "${witData.lastActionPrefix || 'N/A'}" to "${prefix}"`);
+            console.log(`Resetting context...`);
+            newWitData = {
+                ...witData,
+                context: {},
+                lastActionPrefix: prefix,
+            };
+            newRequestData = {
+                ...requestData,
+                context: {},
+            }
+        }
         const { msg, context: newContext = {} } =
-            await _runAction(action, requestData, client.config.actions) || {};
+            await _runAction(action, newRequestData, client.config.actions) || {};
         if (msg) {
             client.config.respondFn(msg);
         }
-        const newConverseData = await client.converse(sessionId, null, newContext);
-        return await _runActionsHelper(client, sessionId, text, newContext,
+        const newConverseData = await client.converse(newWitData.sessionId, null, newWitData.context);
+        return await _runActionsHelper(client, text, newWitData,
                                        botParams, newConverseData, level-1)
     } else {
         console.error('unknown response type', converseData);
@@ -121,6 +150,7 @@ export async function _runActionsHelper(client: Wit, sessionId: string,
 export async function _runAction(actionName: string, actionRequest: ActionRequest,
                                  localActions: {[key: string]: Function})
 {
+    console.log('_runAction: ')
     const requestData = {
         ...actionRequest,
         action: actionName,
@@ -193,8 +223,11 @@ export async function ai(message: DBMessage,
         throw new Error('ai: couldn\'t find the conversation');
     }
 
-    const { witContext = {} } = qres.Items[0];
-    console.log('ai: got context: ', witContext);
+    const witData = Object.assign({
+        sessionId: uuid.v1(),
+        context: {},
+    }, qres.Items[0].witData);
+    console.log('ai: witData: ', witData);
 
     let text = message.text;
     if (message.files) {
@@ -203,15 +236,17 @@ export async function ai(message: DBMessage,
     if (!text) {
         return;
     }
+
+
     const client = _mkClient(botParams.settings.witAccessToken, respondFn);
-    const newWitContext = await _runActions(client, conversationId, text, witContext, botParams);
+    const newWitData = await _runActions(client, text, witData, botParams);
 
     await aws.dynamoPut({
         TableName: DB_TABLE_CONVERSATIONS,
         Item: {
             publisherId,
             conversationId,
-            witContext: newWitContext,
+            witData: newWitData,
         },
     });
 }
