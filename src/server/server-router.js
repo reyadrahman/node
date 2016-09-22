102a9bc276038e0f790ed897a2dbe2bbe102a999
vatre <trean.v@gmail.com>
2016-09-22 19:09:41 +0200

    Web Channel server-side implementation
    #123

--- src/server/server-router.js
+++ src/server/server-router.js
@@ -5,33 +5,35 @@ import render from './server-side-rendering.js';
 import bridge from './client-server-bridge.js';
 import { webhooks } from './channels/all-channels.js';
 import { feedsPeriodicUpdate } from './periodic-tasks.js';
+import { Server as WebSocketServer } from 'ws';
 
-const routes = express.Router();
+export function initializeRoutes(app) {
+    const routes = express.Router();
 
-routes.use('/api', bridge);
+    routes.use('/api', bridge);
 
-routes.use('/webhooks/:publisherId/:botId/:channel', (req, res, next) => {
-    const { channel } = req.params;
-    console.log('server-routes');
-    console.log('channel: ', channel);
-    console.log('publisherId: ', req.params.publisherId);
-    console.log('botId: ', req.params.botId);
+    routes.use('/webhooks/:publisherId/:botId/:channel', (req, res, next) => {
+        const { channel } = req.params;
+        console.log('server-routes');
+        console.log('channel: ', channel);
+        console.log('publisherId: ', req.params.publisherId);
+        console.log('botId: ', req.params.botId);
 
-    if (!webhooks[channel]) {
-        return next(new Error(`Unknown channel: ${channel}`));
-    }
+        if (!webhooks[channel]) {
+            return next(new Error(`Unknown channel: ${channel}`));
+        }
 
-    webhooks[channel](req, res);
-});
+        webhooks[channel](req, res);
+    });
 
-routes.post('/run-periodic-tasks', (req, res, next) => {
-    feedsPeriodicUpdate(req, res);
-});
+    routes.post('/run-periodic-tasks', (req, res, next) => {
+        feedsPeriodicUpdate(req, res);
+    });
 
-routes.use('/', (req, res, next) => {
-    console.log('server-router: /, cookies: ', req.cookies);
-    render(!req.cookies.signedIn, req, res, next);
-});
+    routes.use('/', (req, res, next) => {
+        console.log('server-router: /, cookies: ', req.cookies);
+        render(!req.cookies.signedIn, req, res, next);
+    });
 
-
-export default routes;
+    return new WebSocketServer({ server: app });
+}
