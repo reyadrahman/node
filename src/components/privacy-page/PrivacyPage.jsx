28709ca742bb4e3f2ea7d59c6d47f3419da9daca
Deepiks Dev <support@deepiks.io>
2016-09-26 17:36:39 +0200

    Updated privacy and terms #101

--- src/components/privacy-page/PrivacyPage.jsx
+++ src/components/privacy-page/PrivacyPage.jsx
@@ -10,40 +10,88 @@ const PrivacyPage = React.createClass({
             <div className="privacy-page-comp">
                 <Header i18n={i18n} />
                 <div className="content">
-                    <h1>{strings.privacy}</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non sapien ligula. Etiam massa odio,
-                        condimentum a sapien non, interdum vestibulum sem. Sed pharetra sapien tellus, sed pulvinar risus aliquet
-                        ac. Integer gravida cursus diam, ac interdum eros tincidunt sit amet. Suspendisse in feugiat tortor. Morbi
-                        ex turpis, fermentum non lectus eget, interdum mollis velit. Donec in tincidunt nisl. Nullam ultrices ante
-                        non nisi tincidunt, rhoncus efficitur massa egestas.
-
-                        Morbi rutrum, nunc ac lacinia efficitur, nisi eros blandit tellus, quis suscipit massa tellus at mi. Quisque
-                        sagittis urna at sapien hendrerit, imperdiet euismod nulla euismod. Aliquam erat volutpat. Class aptent
-                        taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur eget mauris
-                        molestie, luctus metus eu, fringilla purus. Aliquam id nunc et augue sagittis elementum. Suspendisse
-                        potenti. Cras sodales justo nunc, a aliquam leo maximus at.
-
-                        Maecenas blandit et quam sit amet accumsan. Sed in risus auctor, sagittis quam in, pretium lectus. Mauris
-                        sed pharetra metus, venenatis lobortis nunc. Fusce malesuada mattis rutrum. Quisque et rhoncus erat. Aliquam
-                        erat volutpat. Praesent ut sapien ex. Duis rutrum maximus magna, et pretium lectus pellentesque eget.
-                        Vivamus est odio, interdum ac cursus a, posuere at arcu. Proin molestie nisl eget est luctus egestas. Duis
-                        non mauris odio.
-
-                        Nullam sagittis quis tellus ut maximus. Integer non gravida nisi. Etiam sed viverra diam. Phasellus id
-                        pharetra nisl. Pellentesque interdum luctus dui. In hac habitasse platea dictumst. Sed eu scelerisque urna.
-                        Nullam molestie volutpat facilisis. Vivamus nec facilisis massa. Nullam non tincidunt neque, faucibus
-                        tincidunt nisl. Nulla at magna non purus placerat rutrum eu sed enim.
-
-                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed volutpat
-                        tincidunt pretium. Nunc volutpat nunc lacus, quis semper lorem finibus eget. Quisque mattis arcu sed sapien
-                        interdum lobortis. Vivamus aliquam odio elit, vel auctor leo scelerisque id. Maecenas ut dui turpis.
-                        Vestibulum quis ligula interdum, molestie diam imperdiet, scelerisque felis. Vivamus id condimentum tellus.
-                        Fusce non odio ipsum. In imperdiet pretium tortor, eget sagittis ipsum tempus ut. Nulla accumsan sagittis
-                        laoreet. Etiam lectus urna, consectetur eu quam a, suscipit malesuada lorem. Praesent vel nisl in tellus
-                        dignissim pretium.
-                    </p>
-                </div>
+<h1>Statement of Rights and Responsibilities</h1>
+<p><em>Date of Last Revision: September 26, 2015</em></p>
+<p></p>
+<p>This Statement of Rights and Responsibilities ("Statement," "Terms," or "SRR") derives from the Deepiks Principles, and is our terms of service that governs our relationship with users and others who interact with Deepiks, as well as Deepiks brands, products and services, which we call the “Deepiks Services” or “Services”. By using or accessing the Deepiks Services, you agree to this Statement, as updated from time to time in accordance with Section 7 below. </p>
+<h2>1- Privacy</h2>
+<p>Your privacy is very important to us. We designed this Policy to make important disclosures about how you can use Deepiks and how we collect and can use your content and information. We encourage you to read this Policy, and to use it to help you make informed decisions. </p>
+<p> </p>
+<h2>2- Sharing Your Content and Information</h2>
+<p>You own all of the content and information you post on Deepiks</p>
+<p>When you delete  content, it is deleted in a manner similar to emptying the recycle bin on a computer. However, you understand that removed content may persist in backup copies for a reasonable period of time (but will not be available to others).</p>
+<p>When you publish content or information using the Public setting, it means that you are allowing everyone, including people off of Deepiks, to access and use that information, and to associate it with you (i.e., your name and profile picture).</p>
+<p>We always appreciate your feedback or other suggestions about Deepiks, but you understand that we may use your feedback or suggestions without any obligation to compensate you for them (just as you have no obligation to offer them).</p>
+<p> </p>
+<h2>3- Safety</h2>
+<p>We do our best to keep Deepiks safe, but we cannot guarantee it. We need your help to keep Deepiks safe, which includes the following commitments by you:</p>
+<p>- You will not post unauthorized commercial communications (such as spam) on Deepiks.</p>
+<p>- You will not collect users' content or information, or otherwise access Deepiks, using automated means (such as harvesting bots, robots, spiders, or scrapers) without our prior permission.</p>
+<p>- You will not engage in unlawful multi-level marketing, such as a pyramid scheme, on Deepiks.</p>
+<p>- You will not upload viruses or other malicious code.</p>
+<p>- You will not solicit login information or access an account belonging to someone else.</p>
+<p>- You will not bully, intimidate, or harass any user.</p>
+<p>- You will not post content that: is hate speech, threatening, or pornographic; incites violence; or contains nudity or graphic or gratuitous violence.</p>
+<p>- You will not develop or operate a third-party application containing alcohol-related, dating or other mature content (including advertisements) without appropriate age-based restrictions.</p>
+<p>- You will not use Deepiks to do anything unlawful, misleading, malicious, or discriminatory.</p>
+<p>- You will not do anything that could disable, overburden, or impair the proper working or appearance of Deepiks, such as a denial of service attack or interference with page rendering or other Deepiks functionality.</p>
+<p>- You will not facilitate or encourage any violations of this Statement or our policies.</p>
+<p> </p>
+<h2>4- Registration and Account Security</h2>
+<p>Here are some commitments you make to us relating to registering and maintaining the security of your account:</p>
+<p>- You will not provide any false personal information on Deepiks, or create an account for anyone other than yourself without permission.</p>
+<p>- You will not create more than one personal account.</p>
+<p>- If we disable your account, you will not create another one without our permission.</p>
+<p>- You will not use Deepiks if you are a convicted sex offender.</p>
+<p>- You will keep your contact information accurate and up-to-date.</p>
+<p>- You will not share your password , let anyone else access your account, or do anything else that might jeopardize the security of your account.</p>
+<p>- You will not transfer your account  to anyone without first getting our written permission.</p>
+<p> </p>
+<h2>5- Protecting Other People's Rights</h2>
+<p>We respect other people's rights, and expect you to do the same.</p>
+<p>You will not post content or take any action on Deepiks that infringes or violates someone else's rights or otherwise violates the law.</p>
+<p>We can remove any content or information you post on Deepiks if we believe that it violates this Statement or our policies.</p>
+<p> </p>
+<h2>6- Mobile and Other Devices</h2>
+<p>We currently provide our mobile services for free, but please be aware that your carrier's normal rates and fees, such as text messaging and data charges, will still apply.</p>
+<p> </p>
+<h2>7- Amendments</h2>
+<p></p>
+<p>We’ll notify you before we make changes to these terms and give you the opportunity to review and comment on the revised terms before continuing to use our Services.</p>
+<p>Your continued use of the Deepiks Services, following notice of the changes to our terms, policies or guidelines, constitutes your acceptance of our amended terms, policies or guidelines.</p>
+<p> </p>
+<h2>8- Termination</h2>
+<p>If you violate the letter or spirit of this Statement, or otherwise create risk or possible legal exposure for us, we can stop providing all or part of Deepiks to you. We will notify you by email or at the next time you attempt to access your account. You may also delete your account or disable your application at any time. </p>
+<p> </p>
+<h2>9- Disputes</h2>
+<p>You will resolve any claim, cause of action or dispute (claim) you have with us arising out of or relating to this Statement or Deepiks exclusively in the Evry Court in France, and you agree to submit to the personal jurisdiction of such courts for the purpose of litigating all such claims. The laws of France will govern this Statement, as well as any claim that might arise between you and us, without regard to conflict of law provisions.</p>
+<p>If anyone brings a claim against us related to your actions, content or information on Deepiks, you will indemnify and hold us harmless from and against all damages, losses, and expenses of any kind (including reasonable legal fees and costs) related to such claim. Although we provide rules for user conduct, we do not control or direct users' actions on Deepiks and are not responsible for the content or information users transmit or share on Deepiks. We are not responsible for any offensive, inappropriate, obscene, unlawful or otherwise objectionable content or information you may encounter on Deepiks. We are not responsible for the conduct, whether online or offline, of any user of Deepiks.</p>
+<p>WE TRY TO KEEP Deepiks UP, BUG-FREE, AND SAFE, BUT YOU USE IT AT YOUR OWN RISK. WE ARE PROVIDING Deepiks AS IS WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. WE DO NOT GUARANTEE THAT Deepiks WILL ALWAYS BE SAFE, SECURE OR ERROR-FREE OR THAT Deepiks WILL ALWAYS FUNCTION WITHOUT DISRUPTIONS, DELAYS OR IMPERFECTIONS. Deepiks IS NOT RESPONSIBLE FOR THE ACTIONS, CONTENT, INFORMATION, OR DATA OF THIRD PARTIES, AND YOU RELEASE US, OUR DIRECTORS, OFFICERS, EMPLOYEES, AND AGENTS FROM ANY CLAIMS AND DAMAGES, KNOWN AND UNKNOWN, ARISING OUT OF OR IN ANY WAY CONNECTED WITH ANY CLAIM YOU HAVE AGAINST ANY SUCH THIRD PARTIES. IF YOU ARE A CALIFORNIA RESIDENT, YOU WAIVE CALIFORNIA CIVIL CODE §1542, WHICH SAYS: A GENERAL RELEASE DOES NOT EXTEND TO CLAIMS WHICH THE CREDITOR DOES NOT KNOW OR SUSPECT TO EXIST IN HIS OR HER FAVOR AT THE TIME OF EXECUTING THE RELEASE, WHICH IF KNOWN BY HIM OR HER MUST HAVE MATERIALLY AFFECTED HIS OR HER SETTLEMENT WITH THE DEBTOR. WE WILL NOT BE LIABLE TO YOU FOR ANY LOST PROFITS OR OTHER CONSEQUENTIAL, SPECIAL, INDIRECT, OR INCIDENTAL DAMAGES ARISING OUT OF OR IN CONNECTION WITH THIS STATEMENT OR Deepiks, EVEN IF WE HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. OUR AGGREGATE LIABILITY ARISING OUT OF THIS STATEMENT OR Deepiks WILL NOT EXCEED THE GREATER OF ONE HUNDRED DOLLARS ($100) OR THE AMOUNT YOU HAVE PAID US IN THE PAST TWELVE MONTHS. APPLICABLE LAW MAY NOT ALLOW THE LIMITATION OR EXCLUSION OF LIABILITY OR INCIDENTAL OR CONSEQUENTIAL DAMAGES, SO THE ABOVE LIMITATION OR EXCLUSION MAY NOT APPLY TO YOU. IN SUCH CASES, Deepiks'S LIABILITY WILL BE LIMITED TO THE FULLEST EXTENT PERMITTED BY APPLICABLE LAW.</p>
+<p> </p>
+<h2>10- Definitions</h2>
+<p></p>
+<p>By "Deepiks" or” Deepiks Services” we mean the features and services we make available, including through (a) our website at www.Deepiks.com and any other Deepiks branded or co-branded websites (including sub-domains, international versions, widgets, and mobile versions); (b) our Platform; (c) social plugins such as the Like button, the Share button and other similar offerings; and (d) other media, brands, products, services, software (such as a toolbar), devices, or networks now existing or later developed. Deepiks reserves the right to designate, in its sole discretion, that certain of our brands, products, or services are governed by separate terms and not this SRR.</p>
+<p>By "Platform" we mean a set of APIs and services (such as content) that enable others, including application developers and website operators, to retrieve data from Deepiks or provide data to us.</p>
+<p>By "information" we mean facts and other information about you, including actions taken by users and non-users who interact with Deepiks.</p>
+<p>By "content" we mean anything you or other users post, provide or share using Deepiks Services.</p>
+<p>By "data" or "user data" or "user's data" we mean any data, including a user's content or information that you or third parties can retrieve from Deepiks or provide to Deepiks through Platform.</p>
+<p>By "use" we mean use, run, copy, publicly perform or display, distribute, modify, translate, and create derivative works of.</p>
+<p>By “Trademarks” we mean the list of trademarks provided here. </p>
+<p> </p>
+<h2>11- Other</h2>
+<p></p>
+<p>This Statement makes up the entire agreement between the parties regarding Deepiks, and supersedes any prior agreements.</p>
+<p>If any portion of this Statement is found to be unenforceable, the remaining portion will remain in full force and effect.</p>
+<p>If we fail to enforce any of this Statement, it will not be considered a waiver.</p>
+<p>Any amendment to or waiver of this Statement must be made in writing and signed by us.</p>
+<p>You will not transfer any of your rights or obligations under this Statement to anyone else without our consent.</p>
+<p>All of our rights and obligations under this Statement are freely assignable by us in connection with a merger, acquisition, or sale of assets, or by operation of law or otherwise.</p>
+<p>Nothing in this Statement shall prevent us from complying with the law.</p>
+<p>This Statement does not confer any third party beneficiary rights.</p>
+<p>We reserve all rights not expressly granted to you.</p>
+<p>You will comply with all applicable laws when using or accessing Deepiks.</p>
+<p></p>
+<p>By using or accessing Deepiks Services, you agree that we can collect and use such content and information in accordance with the Data Policy as amended from time to time. </p>               </div>
             </div>
         );
     }
