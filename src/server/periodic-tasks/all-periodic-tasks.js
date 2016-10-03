474a58f1045c9dbd3a552d0501fd48605be4b7ae
sean shirazi <sean@plumcake.engineer>
2016-10-03 16:09:45 +0300

    Better logging to debug periodic tasks #124

--- src/server/periodic-tasks/all-periodic-tasks.js
+++ src/server/periodic-tasks/all-periodic-tasks.js
@@ -8,12 +8,14 @@ const { CALL_SERVER_LAMBDA_SECRET } = ENV;
 
 export default function periodicTasksUpdate(req: Request, res: Response) {
     const reqSecret = req.header('CALL_SERVER_LAMBDA_SECRET');
+    console.log(`periodicTasksUpdate: reqSecret: ${reqSecret}`);
     if (reqSecret !== CALL_SERVER_LAMBDA_SECRET) {
-        console.log('periodicTasksUpdate: secret header does not match: ', reqSecret);
+        console.log(`periodicTasksUpdate: secret header does not match. ` +
+            `Expected ${CALL_SERVER_LAMBDA_SECRET} but instead got ` +
+            `${reqSecret || '<NOTHING>'}`);
         return res.status(404).send();
     }
     res.send();
-
 
     const now = new Date();
 
