cda1a5e2ad33d9271386c9a5fdba038856e2370e
sean shirazi <sean@plumcake.engineer>
2016-10-12 08:30:31 +0300

    bug fix (decomposeKeys botId_channel_authorizationToken) #175

--- src/server/deepiks-bot/deepiks-bot.js
+++ src/server/deepiks-bot/deepiks-bot.js
@@ -115,7 +115,7 @@ export async function _authorizeUser(
         );
     }
 
-    if (!user || decomposeKeys(user.botId_channel_authorizationToken)[2] !== token) {
+    if (!user || decomposeKeys(user.botId_channel_authorizationToken || '')[2] !== token) {
         return await respondFn(_textToResponseMessage(strings.enterAuthorizationTokenOrEmail));
     }
 
