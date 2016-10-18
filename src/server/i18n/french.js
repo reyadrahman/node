8cd8943dc95d5af8592819060c515ef2743a2c1b
Deepiks Dev <support@deepiks.io>
2016-10-18 10:17:12 +0200

    Correct errors in French translations

--- src/server/i18n/french.js
+++ src/server/i18n/french.js
@@ -5,11 +5,11 @@ export default {
         verificationTokenEmailBodyFn: token => `Voici votre code de vérification: ${token}`,
         verificationTokenEmailSubjectFn: botName => `${botName} - Code de vérification`,
         verificationTokenSentFn: email => `Un nouveau code de vérification a été envoyé à ${email}`,
-        enterEmail: `Veullez entrer votre mail`,
+        enterEmail: `Veuillez entrer votre mail`,
         enterVerificationToken: `Veuillez entrer votre code de vérification`,
         successfullyVerified: `Merci, vous êtes autorisé à dialoguer avec ce bot`,
     },
     errors: {
-        general: 'Désolé, il semble qu'il y ait un problème...',
+        general: 'Désolé, il semble qu\'il y ait un problème...',
     },
 };
