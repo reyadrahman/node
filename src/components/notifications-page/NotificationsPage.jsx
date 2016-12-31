946dc6f68eb84503f73b7f50e19313360971d769
Yurko Fedoriv <yfedoriv@nutritionix.com>
2016-12-31 20:13:45 +0800

    Clean up notifications page ~ resolves #288

--- src/components/notifications-page/NotificationsPage.jsx
+++ src/components/notifications-page/NotificationsPage.jsx
@@ -1,103 +1,80 @@
 import React from 'react';
-// import { Form, Input, Button, ButtonArea, TextArea, SuccessMessage,
-//          ErrorMessage } from '../form/Form.jsx';
 import * as actions from '../../app-state/actions.js';
-// import { Title } from '../modal-box-1/ModalBox1.jsx';
-import { connect } from 'react-redux';
-import { withRouter, Link } from 'react-router';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
 import _ from 'lodash';
-import { Grid, Col, Row, Button, Image, Clearfix,
-         FormGroup, ControlLabel, FormControl,
-         InputGroup, Dropdown } from 'react-bootstrap';
+import {Button, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 
 let NotificationsPage = React.createClass({
     getInitialState() {
         return {
-            message: '',
-            selectedBotId: '',
-            categories: '',
+            message:    '',
+            categories: ''
         };
     },
 
     send(e) {
         e.preventDefault();
-        const { message, selectedBotId, categories } = this.state;
+        const {message, categories} = this.state;
+        let selectedBotId           = this.props.currentUser.selectedBotId;
         if (message && selectedBotId) {
             const cats = categories.split(',').map(x => x.trim()).filter(x => x);
-            const msg = { text: message };
+            const msg = {text: message};
             this.props.sendNotification(selectedBotId, msg, cats);
         }
     },
 
-    onBotChange(e) {
-        this.setState({ selectedBotId: e.target.value });
-    },
-
     onMessageChange(e) {
-        this.setState({ message: e.target.value });
+        this.setState({message: e.target.value});
     },
 
     onCategoriesChange(e) {
-        this.setState({ categories: e.target.value });
+        this.setState({categories: e.target.value});
     },
 
     componentDidMount() {
-        const { currentUser, fetchBots } = this.props;
-        if (currentUser) {
-            fetchBots();
-        }
+        // const { currentUser, fetchBots } = this.props;
+        // if (currentUser) {
+        //     fetchBots();
+        // }
     },
 
     render() {
-        const { className, currentUser: { signedIn, botsState },
-                i18n: { strings: { notificationsPage: strings } },
-        } = this.props;
+        const {
+                  className, currentUser: {signedIn, botsState, selectedBotId},
+                  i18n: {strings: {notificationsPage: strings}},
+              } = this.props;
 
         if (!signedIn) {
             return null;
         }
 
-        const { message, selectedBotId } = this.state;
+        const {message} = this.state;
 
         const fetchingBots = !botsState.hasFetched &&
             <h2 className="fetching-bots">{strings.fetching}</h2>;
 
         const botsAreReady = !_.isEmpty(botsState.bots);
-        let botSelect;
+        let botInfo;
         if (botsAreReady) {
-            botSelect = (
-                <FormGroup controlId="formControlsSelect">
-                    <ControlLabel>Select Bot</ControlLabel>
-                    <FormControl
-                        componentClass="select"
-                        value={selectedBotId}
-                        onChange={this.onBotChange}
-                    >
-                    <option value="" disabled selected>select</option>
-                        {
-                            botsState.bots.map(b => {
-                                return <option value={b.botId}>{b.botName}</option>
-                            })
-                        }
-                    </FormControl>
-                </FormGroup>
-            );
+            let selectedBot = _.find(botsState.bots, {botId: selectedBotId});
 
+            botInfo = <h2>{selectedBot.botName}</h2>;
         }
 
         const emptyBotList = botsState.hasFetched && _.isEmpty(botsState.bots) &&
-            <h2 className="empty-bot-list">You don't have any bots yet</h2>;
+            <h2 className="empty-bot-list">{"You don't have any bots yet"}</h2>;
 
         return (
             <div className={`notifications-page-comp ${className || ''}`}>
                 <h1 className="title">{strings.notifications}</h1>
+                { botInfo }
                 {
                     botsAreReady ? null : (fetchingBots || emptyBotList)
                 }
                 {
                     botsAreReady &&
                     <form className="send-notifications-form">
-                        { botSelect }
                         <FormGroup
                             controlId="categories"
                         >
@@ -142,7 +119,7 @@ NotificationsPage = connect(
         currentUser: state.currentUser,
     }),
     {
-        fetchBots: actions.fetchBots,
+        fetchBots:        actions.fetchBots,
         sendNotification: actions.sendNotification,
     }
 )(NotificationsPage);
